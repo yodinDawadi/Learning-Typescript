c5b3585bbc7b29989b8e2bfca34b949e7c0e1175
Yodin dawadi <yodindawadi10@gmail.com>
2025-01-06 01:43:15 +0545

    Update

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,13 @@
-"use strict";
-const addNumber = (a, b) => {
-    return a + b;
-};
-console.log(addNumber(3, 4));
+// const addNumber=(a:number,b:number)=>{
+//     return a + b;
+// }
+// console.log(addNumber(3,4));
+import express from "express";
+const PORT = 8085;
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello from the server");
+});
+app.listen(PORT, () => {
+    console.log(`Server started at port:${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-const addNumber=(a:number,b:number)=>{
-    return a + b;
-}
+// const addNumber=(a:number,b:number)=>{
+//     return a + b;
+// }
 
-console.log(addNumber(3,4));
+// console.log(addNumber(3,4));
+
+import express from "express";
+const PORT:number = 8085;
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello from the server")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server started at port:${PORT}`);
+})
